84ab7bdde613431b8840d4a66c0179bbf30a5ea9
Guillaume BELDILMI <gbeldilmi@pm.me>
2023-10-25 19:30:47 +0200

    Add sort feature

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -9,10 +9,15 @@ import { Router } from '@angular/router';
   providers: [ManagerService]
 })
 export class ListComponent {
-  tasks: Array<Task>;
-  
+  tasks: Array<[number, Task]>;
+  allTasks: Array<[number, Task]>;
   constructor(private manager: ManagerService, private router: Router) {
-    this.tasks = manager.getTasks();
+    let t = manager.getTasks();
+    this.allTasks = [];
+    for (let i = 0; i < t.length; i++) {
+      this.allTasks.push([i, t[i]]);
+    }
+    this.tasks = this.allTasks;
   }
   details(task_id: number) {
     this.manager.setSelectedTaskId(task_id);
@@ -20,5 +25,24 @@ export class ListComponent {
   }
   create() {
     this.router.navigate(['/create']);
+  }
+  sort() {
+    let t = this.allTasks;
+    let s = (<HTMLInputElement>document.getElementById('status')).value;
+    if (s != 'All') {
+      t = t.filter(x => x[1].status == s);
+    }
+    let p = (<HTMLInputElement>document.getElementById('priority')).value;
+    if (p != 'All') {
+      t = t.filter(x => x[1].priority == p);
+    }
+    let a = (<HTMLInputElement>document.getElementById('attr')).value as keyof Task;
+    let o = (<HTMLInputElement>document.getElementById('order')).value == 'asc' ? -1 : 1;
+    if (a == 'priority') o = -o;
+    t.sort((x, y) => {
+      if (x[1][a] < y[1][a]) return o;
+      return -o;
+    });
+    this.tasks = t;
   }
 }
